85051e2958cff138ebeb9d5c6ee444a8b1d478ba
isaias-silva <zackblackmangold@gmail.com>
2021-10-29 22:33:16 -0300

    surpresa pro dia dos namorados aaa

--- script.js
+++ script.js
@@ -1,1 +1,130 @@
-const _0x3f853a=_0x29f1;(function(_0x4d4d73,_0x39d66a){const _0x10593d=_0x29f1,_0x3ff79e=_0x4d4d73();while(!![]){try{const _0x576221=-parseInt(_0x10593d(0x1f5))/0x1*(-parseInt(_0x10593d(0x1dc))/0x2)+-parseInt(_0x10593d(0x1ea))/0x3*(-parseInt(_0x10593d(0x1e7))/0x4)+parseInt(_0x10593d(0x1f8))/0x5+parseInt(_0x10593d(0x1db))/0x6+-parseInt(_0x10593d(0x1e0))/0x7+parseInt(_0x10593d(0x1e4))/0x8*(parseInt(_0x10593d(0x1ee))/0x9)+parseInt(_0x10593d(0x1f7))/0xa*(-parseInt(_0x10593d(0x1d7))/0xb);if(_0x576221===_0x39d66a)break;else _0x3ff79e['push'](_0x3ff79e['shift']());}catch(_0x50db80){_0x3ff79e['push'](_0x3ff79e['shift']());}}}(_0x49f5,0x68ca3));class Calculadora{constructor(_0x5c9c87){const _0x699438=_0x29f1;this[_0x699438(0x1e3)]=document[_0x699438(0x1e1)](_0x5c9c87);}[_0x3f853a(0x1e3)];[_0x3f853a(0x1e6)]=function(){const _0x43ebd8=_0x3f853a;this[_0x43ebd8(0x1e3)][_0x43ebd8(0x1da)]='';};[_0x3f853a(0x1d8)]=function(){const _0x381cff=_0x3f853a;this[_0x381cff(0x1e3)][_0x381cff(0x1da)]=this[_0x381cff(0x1e3)][_0x381cff(0x1da)][_0x381cff(0x1ed)](0x0,this[_0x381cff(0x1e3)][_0x381cff(0x1da)][_0x381cff(0x1e5)]-0x1);};[_0x3f853a(0x1d2)]=async function(){const _0x31db1f=_0x3f853a,_0xa5e362=await this[_0x31db1f(0x1e9)](this[_0x31db1f(0x1e3)][_0x31db1f(0x1da)]);this[_0x31db1f(0x1e3)]['value']=_0xa5e362;};[_0x3f853a(0x1e9)]=function(_0x1af788){const _0x492a46=_0x3f853a;if(_0x1af788==_0x492a46(0x1dd))return alert('happy\x20birthday\x20for\x20creator!'),'';else{if(_0x1af788=='11.09.2001')return alert(_0x492a46(0x1e2)),'';else{if(_0x1af788==_0x492a46(0x1f3))return alert(_0x492a46(0x1d4)),'';else{if(_0x1af788==_0x492a46(0x1f6))return alert(_0x492a46(0x1fa)),'';else{if(_0x1af788==_0x492a46(0x1d6))return alert(_0x492a46(0x1ef)),'';else{let _0x15185d=_0x1af788['replace']('x','*')[_0x492a46(0x1df)]('^','**')[_0x492a46(0x1df)]('÷','/')[_0x492a46(0x1df)]('%',_0x492a46(0x1eb))['replace']('π',_0x492a46(0x1d3));_0x1af788[_0x492a46(0x1ec)]('√')&&(_0x15185d=_0x15185d[_0x492a46(0x1df)]('√',_0x492a46(0x1f9))+')');if(_0x15185d[_0x492a46(0x1ec)]('()')||_0x15185d[_0x492a46(0x1ec)](_0x492a46(0x1f2))||_0x15185d[_0x492a46(0x1ec)](_0x492a46(0x1d9))||_0x15185d[_0x492a46(0x1ec)]('eval')||_0x15185d[_0x492a46(0x1ec)](_0x492a46(0x1e8))||_0x15185d['includes']('document')||_0x15185d[_0x492a46(0x1ec)]('calculator'))return _0x492a46(0x1de);let _0x1c625f;try{_0x1c625f=eval(_0x15185d);}catch(_0x9f17b8){return alert(_0x9f17b8),_0x1af788;}return _0x1c625f;}}}}}};}function _0x49f5(){const _0x2e5f30=['3641802tOkrVc','118654WfapaO','03.10.2001','NaN','replace','5671932tYZoZw','getElementById','sad\x20day','tela','104mgotIn','length','clear','136yNQpaY','console','extratmath','69801CzcRtq','/100','includes','substring','349911ZXOsZS','❤️❤️❤️❤️❤️PARABÉNS\x20DDZINHA❤️❤️❤️❤️','undefined','fontSize','alert','06.06.1944','Infinity','2tWhrTI','15.11.1889','68260RVlmSn','3834355iRyCrq','Math.sqrt(','res\x27pública!','calc','3.14','you\x27re\x20going\x20to\x20run\x20on\x20the\x20beach,\x20you\x27re\x20going\x20to\x20fight\x20for\x20freedom!','cont','03.02.1975','2497BiIPyG','erase','function','value'];_0x49f5=function(){return _0x2e5f30;};return _0x49f5();}function _0x29f1(_0x417c3f,_0x553c59){const _0x49f5b3=_0x49f5();return _0x29f1=function(_0x29f13c,_0x2cee52){_0x29f13c=_0x29f13c-0x1d2;let _0x4c99cc=_0x49f5b3[_0x29f13c];return _0x4c99cc;},_0x29f1(_0x417c3f,_0x553c59);}const calculator=new Calculadora(_0x3f853a(0x1d5));function press(_0x279e92){const _0x115a92=_0x3f853a;let _0x1eb1b1=0x1e;if(calculator['tela']['value'][_0x115a92(0x1e5)]>=0x13)_0x1eb1b1=0xf;else calculator[_0x115a92(0x1e3)][_0x115a92(0x1da)][_0x115a92(0x1e5)]>=0xe?_0x1eb1b1=0x19:_0x1eb1b1=0x1e;switch(_0x279e92){case'cl':calculator[_0x115a92(0x1e6)]();break;case'er':calculator[_0x115a92(0x1d8)]();break;case _0x115a92(0x1d2):calculator[_0x115a92(0x1d2)]();break;default:calculator[_0x115a92(0x1e3)][_0x115a92(0x1da)]==_0x115a92(0x1f4)||calculator[_0x115a92(0x1e3)]['value']=='NaN'||calculator['tela']['value']==_0x115a92(0x1f0)?calculator['tela'][_0x115a92(0x1da)]=_0x279e92:calculator[_0x115a92(0x1e3)]['value']+=_0x279e92;break;}calculator['tela']['style'][_0x115a92(0x1f1)]=_0x1eb1b1+'px';}
+//codigo fonte é oque está no script.js porém este não está obfuscado//
+//class calculadora
+class Calculadora {
+    constructor(n) {
+        this.tela = document.getElementById(n)
+    }
+//atributo tela
+    tela
+//metodos
+    mmusik= function(theme){
+        if(theme=="amor"){
+            const music = new Audio('./music/love.mp3');
+            music.play();
+        document.body.style.backgroundImage="url('https://t3.ftcdn.net/jpg/02/48/06/52/360_F_248065202_r3vyX7HFCQUN0ilzSirAbk5D1v1ZgyvN.jpg')";
+        this.tela.style.display="none"
+        }
+
+    }
+
+
+    clear = function () {
+
+        this.tela.value = ''
+
+    }
+    erase = function () {
+        this.tela.value = (this.tela.value).substring(0, this.tela.value.length - 1)
+    }
+    calc = async function () {
+
+
+        const result = await this.extratmath(this.tela.value)
+        this.tela.value = result;
+    }
+
+
+    extratmath = function (x) {
+        if (x == '03.10.2001') {
+            alert('happy birthday for creator!')
+            return ''
+        } else if (x == '11.09.2001') {
+            alert('sad day')
+            return ''
+        }
+        else if (x == '06.06.1944') {
+            alert("you're going to run on the beach, you're going to fight for freedom!")
+
+            return ''
+        }
+        else if (x == '15.11.1889') {
+            alert("res'pública!")
+
+            return ''
+        }
+        else if (x == '03.02.1975') {
+            this.mmusik('amor')
+            alert("❤️❤️❤️❤️❤️PARABÉNS DDZINHA❤️❤️❤️❤️")
+            
+            return ''
+        }
+        else {
+            let formated = x.replace('x', '*')
+                .replace('^', '**')
+                .replace('÷', '/')
+                .replace('%', '/100')
+                .replace('π', '3.14')
+               
+
+
+            if (x.includes('√')) {
+                formated = formated.replace('√', 'Math.sqrt(') + ')'
+            }
+
+           
+if(formated.includes('()')||formated.includes('alert')||formated.includes('function')||formated.includes('eval')||formated.includes('console')||formated.includes('document')||formated.includes('calculator')){return 'NaN'}
+
+            let evalformated
+            try {
+                evalformated = eval(formated)
+                
+               
+               
+            } catch (err) {
+                alert(err)
+                return x;
+            }
+            
+            return evalformated
+        }
+    }
+
+}
+//obj principal
+const calculator = new Calculadora('cont')
+
+//função principal
+function press(x) {
+   
+       
+       
+
+    let size = 30
+    if ((calculator.tela.value).length >= 19) {
+        size = 15
+
+    } else if ((calculator.tela.value).length >= 14) {
+        size = 25
+    } else { size = 30 }
+
+    switch (x) {
+        case 'cl':
+            calculator.clear()
+            break
+
+        case 'er':
+            calculator.erase()
+            break
+        case 'calc':
+            calculator.calc()
+            break
+        default:
+            if (calculator.tela.value == 'Infinity' || calculator.tela.value == 'NaN'|| calculator.tela.value == 'undefined') {
+                calculator.tela.value = x;
+            } else {
+                calculator.tela.value += x;
+            }
+            break
+    }
+    calculator.tela.style.fontSize = `${size}px`
+}
